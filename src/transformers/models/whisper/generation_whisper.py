456e9fe51b18509b35cc0414644bebf3a0c7aebd
Anthony Lobko <anthonylobko@gmail.com>
2024-10-01 18:20:52 -0400

    Move stuff to CPU which shouldn't be there anymore

--- src/transformers/models/whisper/generation_whisper.py
+++ src/transformers/models/whisper/generation_whisper.py
@@ -990,7 +990,7 @@ class WhisperGenerationMixin(GenerationMixin):
             if beam_indices is not None and key == "scores":
                 return [v[beam_idx].cpu() for (v, beam_idx) in zip(values, beam_indices[batch_idx][: len(values)])]
             if key in ["scores", "encoder_attentions", "encoder_hidden_states", "logits"]:
-                return [v[batch_idx] for v in values]
+                return [v[batch_idx].cpu() for v in values]
             if key in ["decoder_attentions", "decoder_hidden_states", "cross_attentions"]:
                 return tuple(tuple(w[batch_idx][None].cpu() for w in v) for v in values)
             elif key == "past_key_values":
